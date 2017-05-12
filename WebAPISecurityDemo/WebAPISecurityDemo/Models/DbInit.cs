d08646633eb7c55c50719362ef8b7289d2c011e6
Mike Rissen <mrissen@centriq.com>
2017-05-11 22:00:45 -0500

    Cleanup

--- WebAPISecurityDemo/WebAPISecurityDemo/Models/DbInit.cs
+++ WebAPISecurityDemo/WebAPISecurityDemo/Models/DbInit.cs
@@ -3,7 +3,7 @@ using System.Data.Entity;
 
 namespace WebAPISecurityDemo.Models
 {
-    internal class DbInit : CreateDatabaseIfNotExists<ApplicationDbContext>
+    internal class DbInit : DropCreateDatabaseAlways<ApplicationDbContext>
     {
         protected override void Seed(ApplicationDbContext context)
         {
@@ -18,7 +18,34 @@ namespace WebAPISecurityDemo.Models
                 new Employee() { FirstName = "Wilma", LastName = "Flinstone" },
                 new Employee() { FirstName = "Barney", LastName = "Rubble" },
                 new Employee() { FirstName = "Betty", LastName = "Rubble" },
-            });
+                new Employee() { FirstName = "Michael", LastName = "Tucker" },
+                new Employee() { FirstName = "Morgan", LastName = "Grimes" },
+                new Employee() { FirstName = "Emmett", LastName = "Milbarge" },
+                new Employee() { FirstName = "Harry", LastName = "Tang" },
+                new Employee() { FirstName = "Diane", LastName = "Beckman" },
+                new Employee() { FirstName = "John", LastName = "Casey" },
+                new Employee() { FirstName = "Fernando" },
+                new Employee() { FirstName = "Bunny" },
+                new Employee() { FirstName = "Chuck", LastName = "Bartowski" },
+                new Employee() { FirstName = "Jeffrey", LastName = "Barnes" },
+                new Employee() { FirstName = "Lester", LastName = "Patel" },
+                new Employee() { FirstName = "Skip", LastName = "Johnson" },
+                new Employee() { FirstName = "Anna", LastName = "Wu" },
+                new Employee() { FirstName = "Hannah" },
+                new Employee() { FirstName = "Greta" },
+                new Employee() { FirstName = "Rick", LastName = "Noble" },
+                new Employee() { FirstName = "Victoria", LastName = "Dunwoody" },
+                new Employee() { FirstName = "Moses", LastName = "Finkelstein" },
+                new Employee() { FirstName = "Porter", LastName = "Bauer" },
+                new Employee() { FirstName = "Marvin", LastName = "Gadsby" },
+                new Employee() { FirstName = "Cecil", LastName = "Goldenberg" },
+                new Employee() { FirstName = "Christina", LastName = "James" },
+                new Employee() { FirstName = "Ruben", LastName = "Mango" },
+                new Employee() { FirstName = "Sal", LastName = "Sawaya" },
+                new Employee() { FirstName = "Skye", LastName = "Stoinitz" },
+                new Employee() { FirstName = "Leticia", LastName = "Williams" },
+                new Employee() { FirstName = "Sarah", LastName = "Yang"}
+        });
             context.SaveChanges();
             base.Seed(context);
         }
